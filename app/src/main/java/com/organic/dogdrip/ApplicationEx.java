f3bb846615d3271ad30fc584f42de1f1e76b8c41
fivetrue <fivetrue1101@gmail.com>
2016-04-25 14:48:25 +0900

    Crashlytics 추가.

--- app/src/main/java/com/organic/dogdrip/ApplicationEx.java
+++ app/src/main/java/com/organic/dogdrip/ApplicationEx.java
@@ -2,10 +2,12 @@ package com.organic.dogdrip;
 
 import android.app.Application;
 
+import com.crashlytics.android.Crashlytics;
 import com.facebook.FacebookSdk;
 import com.organic.dogdrip.image.ImageLoadManager;
 import com.organic.dogdrip.manager.UserInfoManager;
 import com.organic.dogdrip.net.NetworkManager;
+import io.fabric.sdk.android.Fabric;
 
 /**
  * Created by kwonojin on 16. 3. 16..
@@ -16,6 +18,7 @@ public class ApplicationEx extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        Fabric.with(this, new Crashlytics());
         NetworkManager.init(this);
         ImageLoadManager.init(NetworkManager.getInstance().getRequestQueue());
         UserInfoManager.init(this);
